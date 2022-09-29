2a2b3ddaf50676dfdb0b78ae8d8e0b5f7b2e8a57
caio-faustino <caioexfaustino.pro@gmail.com>
2022-09-29 19:23:49 -0300

    requisite 01

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // code here
+  if (ids.length === 0) return [];
+  return data.species.filter((elemento) => ids.find((id) => elemento.id === id));
 }
 
 module.exports = getSpeciesByIds;
