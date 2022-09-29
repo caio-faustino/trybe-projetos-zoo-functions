ad63687dac699be7d7fc64e8b083f989b49bd926
caio-faustino <caioexfaustino.pro@gmail.com>
2022-09-29 20:41:07 -0300

    requisite 02

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
+const { species } = data;
+
+function getAnimalsOlderThan(animal, idade) {
   // seu código aqui
+  return species.find(({ name }) => name === animal).residents.every(({ age }) => age >= idade);
 }
 
 module.exports = getAnimalsOlderThan;
