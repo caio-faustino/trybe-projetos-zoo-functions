f837f66be304f292bf6488b6da40321a9c1f17bf
caio-faustino <caioexfaustino.pro@gmail.com>
2022-10-11 11:26:10 -0300

    requisite 08

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,21 @@
 const data = require('../data/zoo_data');
 
+const { prices } = data;
+
 function countEntrants(entrants) {
-  // seu código aqui
+  const child = entrants.filter((entrant) => entrant.age > 0 && entrant.age < 18).length;
+  const adult = entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50).length;
+  const senior = entrants.filter((entrant) => entrant.age >= 50).length;
+  return { child, adult, senior };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!(entrants) || Object.entries(entrants).length === 0) {
+    return 0;
+  }
+  const { child, adult, senior } = countEntrants(entrants);
+  const total = (child * prices.child) + (adult * prices.adult) + (senior * prices.senior);
+  return total;
 }
 
 module.exports = { calculateEntry, countEntrants };
