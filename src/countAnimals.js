6bcbc216c096daaf40c54e1dd430c41823b6dd86
caio-faustino <caioexfaustino.pro@gmail.com>
2022-10-11 10:53:46 -0300

    requisite 05

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+const { species } = data;
+
+function countAnimals(bicho) {
+  if (bicho === undefined) {
+    const list = {};
+
+    species.forEach((elemento) => {
+      list[elemento.name] = elemento.residents.length;
+    });
+    return list;
+  }
+
+  const { sex, specie } = bicho;
+  if (sex) {
+    return species.find((element) => element.name === specie)
+      .residents.filter((resident) => resident.sex === sex).length;
+  }
+  return species.find((elemento) => elemento.name === specie).residents.length;
 }
 
 module.exports = countAnimals;
