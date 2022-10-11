8b276f33460401b7f279284cdb8ff32c2320f430
caio-faustino <caioexfaustino.pro@gmail.com>
2022-10-11 10:26:06 -0300

    requisite 04

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,18 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
-  // seu código aqui
+  return employees.some(({ managers }) => managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    const listEmployees = employees.filter((elemento) => elemento.managers.includes(managerId));
+    const notes = listEmployees.map((gerente) => `${gerente.firstName} ${gerente.lastName}`);
+    return notes;
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
